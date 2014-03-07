b49420d0cbd01bc96559232a0b00a0ef1780a5c5
Alejandro Alcalde <algui91@gmail.com>
2014-03-07 13:27:46 +0100

    Added begginer tutorial from ROS wiki

---
+++ beginner_tutorials/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/hkr/Desarrollo/catkin_ws/src/beginner_tutorials/gtest
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
